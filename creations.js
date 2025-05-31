ff9fe6bf81561e97461ef8a36f519eea30aae264
mrcprlx <mrcprlx@gmail.com>
2025-05-30 21:32:16 -0400

    Fix creator field to render multiple URLs as separate links with line breaks

--- creations.js
+++ creations.js
@@ -126,9 +126,11 @@ function renderCreations() {
         // Parse creator field for multiple URLs
         let creatorContent = creation.creator || '';
         if (creatorContent) {
-            const urlRegex = /(https?:\/\/[^\s]+)/g;
+            const urlRegex = /^(https?:\/\/[^\s]+)$/;
             creatorContent = creatorContent
                 .split('\n')
+                .map(line => line.trim())
+                .filter(line => line.length > 0)
                 .map(line => {
                     if (urlRegex.test(line)) {
                         return `<a href="${line}" class="author-link" target="_blank">${line}</a>`;
